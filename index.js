9757eead4f70540858f87db420f65185db244207
Priyal Raj <priyalraj99@gmail.com>
2023-06-03 09:36:50 +0530

    connect2mongodb Module Completed

---
+++ index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+// Just Use MONGO_URI = Your MongoURI In .env but without '' String
+const userMongoDBLink = process.env.MONGO_URI;
+
+async function connect2MongoDB(databaseName) {
+    // If/Else Is Used To Check Whether Database Name Is Defined Or Not
+    try {
+        const MongoDBUrlWithDB = userMongoDBLink + databaseName;
+        // Connecting To The MogoDB
+        await mongoose.connect(MongoDBUrlWithDB);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function disconnect2MongoDB() {
+    try {
+        await mongoose.disconnect();
+        return true;
+        // return console.log("Disconnected from MongoDB! 👋");
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports = { connect2MongoDB, disconnect2MongoDB };
