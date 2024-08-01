67aaca2d8dd0bb9367fdf3838f60f17de621987c
Priyal Raj <priyalraj99@gmail.com>
2024-08-01 09:32:35 +0530

    Updated to Mongoose 8.5.2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+        "node": true,
+        "es6": true
     },
     "extends": "eslint:recommended",
     "overrides": [
